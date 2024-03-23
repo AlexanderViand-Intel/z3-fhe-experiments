7f0a9533eb805623b1f858d91e97458b4bd1aa08
Alexander <alexander.viand@intel.com>
2024-03-23 21:00:07 +0000

    attempts that time out :)

--- main.py
+++ main.py
@@ -7,6 +7,33 @@ def matvecmul(M, v):
     return [sum(M[i][j] * v[j] for j in range(len(v))) for i in range(len(M))]
 
 
+def svecreduceadd(u, n, assumptions):
+    ps = FreshConst(ArraySort(IntSort(), IntSort()), "partialsum")
+    i = Int("i")
+    assumptions.append(ps[0] == u[0])
+    assumptions.append(
+        ForAll([i], Implies(And(i >= 1, i < n), ps[i] == ps[i - 1] + u[i]))
+    )
+    return ps[n - 1]
+
+
+def smatvecmul(M, v, n, assumptions):
+    out = FreshConst(ArraySort(IntSort(), ArraySort(IntSort(), IntSort())), "out")
+    i = Int("i")
+    j = Int("j")
+
+    assumptions.append(
+        ForAll(
+            [i],
+            Implies(
+                And(i >= 0, i < n),
+                out[i] == Sum([M[i][j] * v[j] for j in range(len(v))]),
+            ),
+        )
+    )
+    return out
+
+
 def vecadd(u, v):
     return [a + b for a, b in zip(u, v)]
 
@@ -35,7 +62,8 @@ def svecrot(u, r, n, assumptions):
     )
     return out
 
-def svecmap2(f, fname, u, v, r, n, assumptions):
+
+def svecmap2(f, fname, u, v, n, assumptions):
     out = FreshConst(ArraySort(IntSort(), IntSort()), fname)
     i = Int("i")
     assumptions.append(
@@ -43,11 +71,14 @@ def svecmap2(f, fname, u, v, r, n, assumptions):
     )
     return out
 
+
 def svecadd(u, v, n, assumptions):
     return svecmap2(lambda a, b: a + b, "add", u, v, n, assumptions)
 
+
 def svecmul(u, v, n, assumptions):
     return svecmap2(lambda a, b: a * b, "mul", u, v, n, assumptions)
+
 
 def sveceq(u, v, n):
     i = Int("i")
@@ -55,14 +86,24 @@ def sveceq(u, v, n):
 
 
 def symbolic():
-    n = Int("n")
-    input = FreshConst(ArraySort(IntSort(), IntSort()), "in")
+    n = 2**14
+    input = FreshConst(ArraySort(IntSort(), IntSort()), "in1")
+    input2 = FreshConst(ArraySort(IntSort(), IntSort()), "in2")
     r = Int("r")
     assumptions = [r < n, r >= 0, n > 0]
     rot = svecrot(input, r, n, assumptions)
-    rot2 = svecrot(rot, -r, n, assumptions)
-    correctness = sveceq(input, rot2, n)
-    prove(Implies(And(*assumptions), correctness))
+    # rot2 = svecrot(rot, -r, n, assumptions)
+    # ab = svecmul(input, input2, n, assumptions)
+    # ba = svecmul(input2, input, n, assumptions)
+    # correctness = sveceq(input, rot2, n)
+    correctness = svecreduceadd(input, n, assumptions) == svecreduceadd(
+        rot, n, assumptions
+    )
+    to_prove = Implies(And(*assumptions), correctness)
+    s = Solver()
+    s.add(Not(to_prove))
+    print(s.to_smt2())
+    # prove(to_prove)
 
 
 def concrete():
